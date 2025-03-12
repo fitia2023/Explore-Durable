aed7ff6aea17eab879b86edf2352451ec0776a9c
fitia <fitiandrianarisoa@gmail.com>
2025-03-12 17:24:25 +0100

    initiation dossier

---
+++ backend-explore-durable/src/test/java/fr/explore_durable/backend/explore/durable/ExploreDurableApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.explore_durable.backend.explore.durable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreDurableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
